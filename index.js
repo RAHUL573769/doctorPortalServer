bbe93a5a475c2ca4f351e2a7f249f150c7ef9761
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-05-12 07:27:09 +0600

    mongo connected

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://portal:x1bvjGOWLDi1Hy40@cluster0.oza9o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+console.log(uri);
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
